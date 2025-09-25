345fbd1db978d9b0bec39e16796c15f4264c93a2
ltfoster044 <ltfoster044@gmail.com>
2025-09-25 13:26:50 -0400

    Update index.tsx

--- src/src/index.tsx
+++ src/src/index.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Website from "./Website";
-import "./index.css";
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/png" href="/logo.png" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Luke Electric</title>
+  </head>
+  <body class="bg-black text-white">
+    <div id="root"></div>
+    <script type="module" src="/src/index.tsx"></script>
+  </body>
+</html>
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <Website />
-  </React.StrictMode>
-);
